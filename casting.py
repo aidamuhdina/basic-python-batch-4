41e57fc06f19aba0fb0c5d86cc2d4bc58c0cc5ce
aidamuhdina <aidamuhdina@gmail.com>
2021-02-13 14:35:41 +0700

    basic python pertemuan 1

---
+++ casting.py
@@ -0,0 +1,18 @@
+a = 2.8
+print(type(a))
+
+#casting int
+b = int(a)
+print(type(b))
+print(b)
+
+c = 2
+d = float(c)
+print(type(d))
+
+e = str(c)
+print(type(e))
+
+f = "aida"
+g = float(f)
+print(type(g))
---
+++ math.py
@@ -0,0 +1,26 @@
+x = 10
+y = 3
+
+#tambah
+z1 = x+y
+print("Hasil pertambahan {} + {} = {}".format(x,y,z1))
+
+#kurang
+z2 = x-y
+print("Hasil pengurangan {} - {} = {}".format(x,y,z2))
+
+#kali
+z3 = x*y
+print("Hasil perkalian {} * {} = {}".format(x,y,z3))
+
+#bagi
+z4 = x/y
+print("Hasil pembagian {} / {} = {}".format(x,y,z4))
+
+#modulo
+z5 = x%y
+print("Hasil modulo {} % {} = {}".format(x,y,z5))
+
+#pangkat
+z6 = x**y
+print("Hasil pangkat {} ^ {} = {}".format(x,y,z6))
