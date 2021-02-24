d8c31ea40897a9db508547a2a7f9a60d308d6ed1
aidamuhdina <aidamuhdina@gmail.com>
2021-02-24 13:34:38 +0700

    tugas 2

---
+++ Tugas-2/Soal-1.py
@@ -0,0 +1,32 @@
+kontak = []
+
+def tambah_kontak():
+    nama = input("Nama : ")
+    nomor = input("Nomor : ")
+    kontak.append({"nama":nama, "nomor":nomor})
+    print("Kontak berhasil ditambahkan.")
+
+def list_kontak():
+    print("\nDaftar Kontak:")
+    for info in kontak:
+        print("Nama : {}".format(info["nama"]))
+        print("Nomor : {}\n".format(info["nomor"]))
+    
+
+print("Selamat datang!")
+while True:
+    print('\n---Menu---\n1. Daftar kontak\n2. Tambah kontak\n3. Keluar')
+    perintah = input("Pilih menu : ")
+    
+    if perintah == "1":
+        if kontak == []:
+            print("Kontak kosong!")
+        else:
+            list_kontak()
+    elif perintah == "2":
+        tambah_kontak()
+    elif perintah == "3":
+        print("Program selesai, sampai jumpa!")
+        break
+    else:
+        print("Menu tidak tersedia.")
